72bb3fddb6c3a5f902984b637fa901844c816934
Mark Fang <mark.fang.mo@gmail.com>
2023-01-09 22:45:23 -0600

    searching changes

--- src/pages/Home/components/Recent.tsx
+++ src/pages/Home/components/Recent.tsx
@@ -1,10 +1,16 @@
 import { ClockIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "../../../components/Card";
 import useFolder from "../../../hooks/useFolder";
 
 export default function Recent() {
   const { getRecentEntries } = useFolder();
-  console.log(getRecentEntries())
+  const [history, setHistory] = useState<any[]>([]);
+
+  useEffect(() => {
+    getRecentEntries().then((value) => setHistory(value));
+  }, []);
 
   return (
     <div className="flex flex-col max-w-2xl">
@@ -15,10 +21,29 @@ export default function Recent() {
         </div>
       </div>
 
-      <div className="flex flex-col space-y-2 mt-4">
+      <div className="flex flex-col space-y-2 mt-4 mb-4">
         <h2 className="text-gray-500">
           Browse your recently opened journal entries.
         </h2>
+        {history.map((result, index) => {
+          if (index >= 3) return;
+
+          return (
+            <Link key={result.id} to={`/editor/${result.fileName}`}>
+              <Card>
+                <label className="rounded-full text-md">
+                  {result.readableDate}
+                </label>
+                <h1 className="text-2xl">
+                  {result.title === "" ? "No title." : result.title}
+                </h1>
+                <label className="rounded-full py-0.5 px-4 bg-gray-600 text-xxs">
+                  {result.id}
+                </label>
+              </Card>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/Home/components/Search.tsx
+++ src/pages/Home/components/Search.tsx
@@ -1,12 +1,17 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import React, { useEffect, useState } from "react";
+import { MagnifyingGlassIcon, XCircleIcon } from "@heroicons/react/24/solid";
+import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFolder from "../../../hooks/useFolder";
 import Card from "../../../components/Card";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function Search() {
   const [searchValue, setSearchValue] = useState<string>("");
   const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
   const { searchJournalEntries } = useFolder();
 
@@ -19,48 +24,102 @@ export default function Search() {
 
   return (
     <div className="flex flex-col max-w-2xl">
-      <form className="flex flex-col space-y-2">
-        <div className="flex space-x-2 items-center">
-          <label className="text-xl">Search</label>
-          <div className="bg-black p-1.5 rounded-lg">
-            <MagnifyingGlassIcon className="h-6 w-6 fill-white" />
+      <div className="flex items-center justify-center">
+        <button type="button" onClick={openModal}>
+          <div className="flex items-center space-x-1.5 btn btn-black">
+            <h1>Search</h1>
+            <MagnifyingGlassIcon className="h-6 w-6" />
           </div>
-        </div>
-        <div className="flex space-x-3">
-          <input
-            placeholder="Search for keywords, title, or date (ex. January 01, 2000)"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            className="w-full input"
-          />
-          <button onClick={handleSearch} className="btn btn-cosmic px-4">
-            Search
-          </button>
-        </div>
-      </form>
-      <div className="flex flex-col space-y-2 mt-4">
-        {searchResults ? (
-          searchResults.map((result) => {
-            return (
-              <Link key={result.id} to={`/editor/${result.fileName}`}>
-                <Card>
-                  <label className="rounded-full text-md">
-                    {result.readableDate}
-                  </label>
-                  <h1 className="text-2xl">
-                    {result.title === "" ? "No title." : result.title}
-                  </h1>
-                  <label className="rounded-full py-0.5 px-4 bg-gray-600 text-xxs">
-                    {result.id}
-                  </label>
-                </Card>
-              </Link>
-            );
-          })
-        ) : (
-          <p className={`text-gray-300`}>No results</p>
-        )}
+        </button>
       </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10 " onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-150"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-150"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-900  bg-opacity-50" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-150"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-150"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className=" max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex justify-end">
+                    <button type="button" onClick={closeModal}>
+                      <XCircleIcon className="h-8 w-8" />
+                    </button>
+                  </div>
+                  <form className="flex flex-col space-y-2">
+                    <div className="flex space-x-2 items-center">
+                      <label className="text-xl">Search</label>
+                      <div className="bg-black p-1.5 rounded-lg">
+                        <MagnifyingGlassIcon className="h-6 w-6 fill-white" />
+                      </div>
+                    </div>
+                    <div className="flex space-x-3">
+                      <input
+                        placeholder="Search for keywords, title, or date (ex. January 01, 2000)"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        className="w-full input"
+                      />
+                      <button
+                        onClick={handleSearch}
+                        className="btn btn-cosmic px-4"
+                      >
+                        Search
+                      </button>
+                    </div>
+                  </form>
+                  <div className="flex flex-col space-y-2 mt-4">
+                    {searchResults ? (
+                      searchResults.map((result) => {
+                        return (
+                          <Link
+                            key={result.id}
+                            to={`/editor/${result.fileName}`}
+                          >
+                            <Card>
+                              <label className="rounded-full text-md">
+                                {result.readableDate}
+                              </label>
+                              <h1 className="text-2xl">
+                                {result.title === ""
+                                  ? "No title."
+                                  : result.title}
+                              </h1>
+                              <label className="rounded-full py-0.5 px-4 bg-gray-600 text-xxs">
+                                {result.id}
+                              </label>
+                            </Card>
+                          </Link>
+                        );
+                      })
+                    ) : (
+                      <p className={`text-gray-300`}>No results</p>
+                    )}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 }
